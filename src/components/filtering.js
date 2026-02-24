02be2fd42cc6cea03ba36249bc2ad7b746babcc3
Kamilla Tozhiboeva <tozhiboeva.kamilla@yandex.ru>
2026-02-24 17:53:05 +0300

    проект готов

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,31 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+        .forEach((elementName) => {
+            if (elements[elementName].options.length === 1) {
+                elements[elementName].append(
+                    ...Object.values(indexes[elementName])
+                        .map(name => {
+                            return new Option(name)
+                        })
+                )
+            }
+        })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-
+        if (action && action.name === 'clear') {
+          const input = document.querySelector('input [name = "' + action.dataset.field + '"]');
+          input.value = '';
+          state[action.dataset.field] = '';
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
+
+
--- src/components/table.js
+++ src/components/table.js
@@ -13,13 +13,50 @@ export function initTable(settings, onAction) {
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
+    before.reverse().forEach((subName) => {                            // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+    root.container.prepend(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
+
+    after.forEach(subName => {                            // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+    root.container.append(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+});
+
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener("change", () => {
+        onAction();
+    });
+
+    root.container.addEventListener("reset", () => {
+        setTimeout(onAction);
+    });
+
+    root.container.addEventListener("submit", (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+            const row = cloneTemplate(rowTemplate);
 
-    return {...root, render};
+            Object.keys(item).forEach((key) => { //перебираем все ключи
+                if (row.elements[key]) { 
+                    const el = row.elements[key];
+                    if (el.tagName === "INPUT" || el.tagName === "SELECT") {
+                        el.value = item[key];
+                    } else {
+                        el.textContent = item[key]; //проверяем, чтобы инпут или селект был инпутом и селектом. Если тру, то присваем значение в value, 
+                    }
+                }
+            });
+
+            return row.container; //возвращаем row с conteiner для map 
+        });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
+
+  return { ...root, render };
 }
